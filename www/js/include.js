3c19293c869864864516cec3d9ac344afd17a325
suzq <hekysu@qq.com>
2014-12-23 16:18:26 +0800

    (*) 备份到GitHub

---
+++ www/js/include.js
@@ -0,0 +1,26 @@
+//document.write('<link type="text/css" rel="stylesheet/less" href="css/style.less"/>');
+//document.write('<link type="text/css" rel="stylesheet" href="css/core.css"/>');
+//document.write('<link type="text/css" rel="stylesheet" href="css/impromptu.css"/>');
+//document.write('<link type="text/css" rel="stylesheet/less" href="css/widget.less"/>');
+//document.write('<link type="text/css" rel="stylesheet" href="css/widget.css"/>');
+//document.write('<script type="text/javascript" src="js/less-1.3.3.min.js"></script>');
+document.write('<script type="text/javascript" src="js/zepto.js"></script>');
+//document.write('<script type="text/javascript" src="js/selector.js"></script>');
+////document.write('<script type="text/javascript" src="js/event.js"></script>');
+//document.write('<script type="text/javascript" src="js/fx.js"></script>');
+//document.write('<script type="text/javascript" src="js/fx_methods.js"></script>');
+//document.write('<script type="text/javascript" src="js/zepto-slide-transition.js"></script>');
+//document.write('<script type="text/javascript" src="js/zepto.leanModal.js"></script>');
+//document.write('<script type="text/javascript" src="js/touch.js"></script>');
+//document.write('<script type="text/javascript" src="js/ui.js"></script>');
+//document.write('<script type="text/javascript" src="js/mustache.js"></script>');
+//document.write('<script type="text/javascript" src="js/plugin.js"></script>');
+//document.write('<script type="text/javascript" src="js/local.js"></script>');
+
+
+
+
+//document.write('<script type="text/javascript" src="js/env.js"></script>');
+//document.write('<script type="text/javascript" src="js/cordova-ios-2.9.0.js"></script>');
+document.write('<script type="text/javascript" src="js/ios-plugin.js"></script>');
+
